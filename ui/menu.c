d1324b83016418481bdd2fca13ededd80dac6944
troilus <1055100+troilus@users.noreply.github.com>
2024-10-16 09:36:58 +0800

    Update menu.c 精简不常用菜单项-L175

--- ui/menu.c
+++ ui/menu.c
@@ -46,81 +46,81 @@ const t_menu_item MenuList[] =
     {"RxCTCS",      MENU_R_CTCS        }, // was "R_CTCS"
     {"TxDCS",       MENU_T_DCS         }, // was "T_DCS"
     {"TxCTCS",      MENU_T_CTCS        }, // was "T_CTCS"
-    {"TxODir",      MENU_SFT_D         }, // was "SFT_D"
+    {"TxDir",       MENU_SFT_D         }, // was "SFT_D"
     {"TxOffs",      MENU_OFFSET        }, // was "OFFSET"
     {"W/N",         MENU_W_N           },
-#ifndef ENABLE_FEAT_F4HWN
-    {"Scramb",      MENU_SCR           }, // was "SCR"
-#endif
-    {"BusyCL",      MENU_BCL           }, // was "BCL"
+//#ifndef ENABLE_FEAT_F4HWN
+//    {"Scramb",      MENU_SCR           }, // was "SCR"
+//#endif
+    {"BzLock",      MENU_BCL           }, // was "BCL"
     {"Compnd",      MENU_COMPAND       },
     {"Mode",        MENU_AM            }, // was "AM"
 #ifdef ENABLE_FEAT_F4HWN
     {"TXLock",      MENU_TX_LOCK       }, 
 #endif
-    {"ScAdd1",      MENU_S_ADD1        },
-    {"ScAdd2",      MENU_S_ADD2        },
-    {"ScAdd3",      MENU_S_ADD3        },
+//    {"ScAdd1",      MENU_S_ADD1        },
+//    {"ScAdd2",      MENU_S_ADD2        },
+//    {"ScAdd3",      MENU_S_ADD3        },
     {"ChSave",      MENU_MEM_CH        }, // was "MEM-CH"
     {"ChDele",      MENU_DEL_CH        }, // was "DEL-CH"
     {"ChName",      MENU_MEM_NAME      },
 
-    {"SList",       MENU_S_LIST        },
-    {"SList1",      MENU_SLIST1        },
-    {"SList2",      MENU_SLIST2        },
-    {"SList3",      MENU_SLIST3        },
-    {"ScnRev",      MENU_SC_REV        },
-#ifdef ENABLE_NOAA
-    {"NOAA-S",      MENU_NOAA_S        },
-#endif
+//    {"SList",       MENU_S_LIST        },
+//    {"SList1",      MENU_SLIST1        },
+//    {"SList2",      MENU_SLIST2        },
+//    {"SList3",      MENU_SLIST3        },
+    {"ScSTOP",      MENU_SC_REV        },
+//#ifdef ENABLE_NOAA
+//    {"NOAA-S",      MENU_NOAA_S        },
+//#endif
     {"F1Shrt",      MENU_F1SHRT        },
     {"F1Long",      MENU_F1LONG        },
     {"F2Shrt",      MENU_F2SHRT        },
     {"F2Long",      MENU_F2LONG        },
     {"M Long",      MENU_MLONG         },
 
-    {"KeyLck",      MENU_AUTOLK        }, // was "AUTOLk"
-    {"TxTOut",      MENU_TOT           }, // was "TOT"
+//    {"KeyLck",      MENU_AUTOLK        }, // was "AUTOLk"
+//    {"TxTOut",      MENU_TOT           }, // was "TOT"
     {"BatSav",      MENU_SAVE          }, // was "SAVE"
     {"BatTxt",      MENU_BAT_TXT       },
     {"Mic",         MENU_MIC           },
-#ifdef ENABLE_AUDIO_BAR
-    {"MicBar",      MENU_MIC_BAR       },
-#endif
+//#ifdef ENABLE_AUDIO_BAR
+//    {"MicBar",      MENU_MIC_BAR       },
+//#endif
     {"ChDisp",      MENU_MDF           }, // was "MDF"
     {"POnMsg",      MENU_PONMSG        },
     {"BLTime",      MENU_ABR           }, // was "ABR"
-    {"BLMin",       MENU_ABR_MIN       },
+//    {"BLMin",       MENU_ABR_MIN       },
     {"BLMax",       MENU_ABR_MAX       },
-    {"BLTxRx",      MENU_ABR_ON_TX_RX  },
+//    {"BLTxRx",      MENU_ABR_ON_TX_RX  },
     {"Beep",        MENU_BEEP          },
-#ifdef ENABLE_VOICE
-    {"Voice",       MENU_VOICE         },
-#endif
+//#ifdef ENABLE_VOICE
+//    {"Voice",       MENU_VOICE         },
+//#endif
     {"Roger",       MENU_ROGER         },
     {"STE",         MENU_STE           },
     {"RP STE",      MENU_RP_STE        },
     {"1 Call",      MENU_1_CALL        },
-#ifdef ENABLE_ALARM
-    {"AlarmT",      MENU_AL_MOD        },
-#endif
-#ifdef ENABLE_DTMF_CALLING
-    {"ANI ID",      MENU_ANI_ID        },
-#endif
-    {"UPCode",      MENU_UPCODE        },
-    {"DWCode",      MENU_DWCODE        },
-    {"PTT ID",      MENU_PTT_ID        },
-    {"D ST",        MENU_D_ST          },
-#ifdef ENABLE_DTMF_CALLING
-    {"D Resp",      MENU_D_RSP         },
-    {"D Hold",      MENU_D_HOLD        },
-#endif
-    {"D Prel",      MENU_D_PRE         },
-#ifdef ENABLE_DTMF_CALLING
-    {"D Decd",      MENU_D_DCD         },
-    {"D List",      MENU_D_LIST        },
-#endif
-    {"D Live",      MENU_D_LIVE_DEC    }, // live DTMF decoder
+//#ifdef ENABLE_ALARM
+//    {"AlarmT",      MENU_AL_MOD        },
+//#endif
+//#ifdef ENABLE_DTMF_CALLING
+//    {"ANI ID",      MENU_ANI_ID        },
+//#endif
+//    {"UPCode",      MENU_UPCODE        },
+//    {"DWCode",      MENU_DWCODE        },
+//    {"PTT ID",      MENU_PTT_ID        },
+//    {"D ST",        MENU_D_ST          },
+//#ifdef ENABLE_DTMF_CALLING
+//    {"D Resp",      MENU_D_RSP         },
+//    {"D Hold",      MENU_D_HOLD        },
+//#endif
+//    {"D Prel",      MENU_D_PRE         },
+//#ifdef ENABLE_DTMF_CALLING
+//    {"D Decd",      MENU_D_DCD         },
+//    {"D List",      MENU_D_LIST        },
+//#endif
+//    {"D Live",      MENU_D_LIVE_DEC    }, // live DTMF decoder
 #ifndef ENABLE_FEAT_F4HWN
     #ifdef ENABLE_AM_FIX
         {"AM Fix",      MENU_AM_FIX        },
@@ -134,14 +134,14 @@ const t_menu_item MenuList[] =
 #else
     {"BatVol",      MENU_VOL           }, // was "VOL"
 #endif
-    {"RxMode",      MENU_TDR           },
+    {"DWMode",      MENU_TDR           },
     {"Sql",         MENU_SQL           },
 #ifdef ENABLE_FEAT_F4HWN
     {"SetPwr",      MENU_SET_PWR       },
-    {"SetPtt",      MENU_SET_PTT       },
-    {"SetTot",      MENU_SET_TOT       },
-    {"SetEot",      MENU_SET_EOT       },
-    {"SetCtr",      MENU_SET_CTR       },
+//    {"SetPtt",      MENU_SET_PTT       },
+//    {"SetTot",      MENU_SET_TOT       },
+//    {"SetEot",      MENU_SET_EOT       },
+//    {"SetCtr",      MENU_SET_CTR       },
     {"SetInv",      MENU_SET_INV       },
     {"SetLck",      MENU_SET_LCK       },
     {"SetMet",      MENU_SET_MET       },
@@ -154,18 +154,18 @@ const t_menu_item MenuList[] =
     // hidden menu items from here on
     // enabled if pressing both the PTT and upper side button at power-on
     {"F Lock",      MENU_F_LOCK        },
-#ifndef ENABLE_FEAT_F4HWN
-    {"Tx 200",      MENU_200TX         }, // was "200TX"
-    {"Tx 350",      MENU_350TX         }, // was "350TX"
-    {"Tx 500",      MENU_500TX         }, // was "500TX"
-#endif
-    {"350 En",      MENU_350EN         }, // was "350EN"
-#ifndef ENABLE_FEAT_F4HWN
-    {"ScraEn",      MENU_SCREN         }, // was "SCREN"
-#endif
-#ifdef ENABLE_F_CAL_MENU
-    {"FrCali",      MENU_F_CALI        }, // reference xtal calibration
-#endif
+//#ifndef ENABLE_FEAT_F4HWN
+//    {"Tx 200",      MENU_200TX         }, // was "200TX"
+//    {"Tx 350",      MENU_350TX         }, // was "350TX"
+//    {"Tx 500",      MENU_500TX         }, // was "500TX"
+//#endif
+//    {"350 En",      MENU_350EN         }, // was "350EN"
+//#ifndef ENABLE_FEAT_F4HWN
+//    {"ScraEn",      MENU_SCREN         }, // was "SCREN"
+//#endif
+//#ifdef ENABLE_F_CAL_MENU
+//    {"FrCali",      MENU_F_CALI        }, // reference xtal calibration
+//#endif
     {"BatCal",      MENU_BATCAL        }, // battery voltage calibration
     {"BatTyp",      MENU_BATTYP        }, // battery type 1600/2200mAh
     {"Reset",       MENU_RESET         }, // might be better to move this to the hidden menu items ?
