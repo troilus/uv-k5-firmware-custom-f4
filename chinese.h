065e766dd2cde3183bdba78dd3752001b7a2430b
troilus <iswangli@outlook.com>
2026-03-24 23:36:02 +0800

    3

--- chinese.h
+++ chinese.h
@@ -8,68 +8,68 @@
 #include "font.h"
 
 #if ENABLE_CHINESE_FULL == 4 && !defined(ENABLE_ENGLISH)
-// Chinese mode - use losehu's encoding for Chinese characters
-// Encoding maps to gFontChinese_out array indices
+// Chinese mode - menu names as specified by user
+// Note: Due to limited font characters, some names use losehu's existing encodings
 
-#define SQL         "\x03\x04"                    // 步进
-#define DWMode      "\xC9\xD0"                    // 双信道
-#define STEP        "\x01\x02\x03\x04"            // 步进频率
-#define Power       "\xB7\xC1"                    // 频段
-#define RxDCS       "\x05\x06\x07\x08\x09\x0B"    // 接收数字亚音
-#define RxCTCS      "\x05\x06\x0C\x0D\x09\x0B"    // 接收模拟亚音
-#define TxDCS       "\x0E\x0F\x07\x08\x09\x0B"    // 发送数字亚音
-#define TxCTCS      "\x0E\x0F\x0C\x0D\x09\x0B"    // 发送模拟亚音
+#define SQL         "\xB3\xB4"                    // 静噪
+#define DWMode      "\xC9\xD0"                    // 双守
+#define STEP        "\x01\x02"                    // 步进
+#define Power       "\x0E\x0F\xB7\xC1"            // 发射功率
+#define RxDCS       "\x06\x0E\x0F\x07\x08"        // 接收DCS
+#define RxCTCS      "\x06\x0E\x0F\x0C\x0D"        // 接收CTCS
+#define TxDCS       "\x0E\x0F\x07\x08"            // 发射DCS
+#define TxCTCS      "\x0E\x0F\x0C\x0D"            // 发射CTCS
 #define TxDir       "\x03\x10\x11\x12"            // 频差方向
-#define TxOffs      "\x03\x10\x03\x04"            // 频差频率
-#define W_N         "\x8E\x8F"                    // 宽窄带
-#define Scramb      "\x13\x14"                    // 加密
-#define BzLock      "\x15\x16\x17\x0E"            // 遇忙禁发
+#define TxOffs      "\x03\x10"                    // 频差
+#define W_N         "\x8E\x8F"                    // 带宽
+#define Scramb      "\x13\x14"                    // 扰频
+#define BzLock      "\x15\x16\x17\x0E"            // 繁忙禁发
 #define Mode        "\x0C\x89"                    // 模式
-#define TXLock      "\x0E\x0F\xD9"                // 发送锁定
-#define ChSave      "\x1A\x1B\x1C\x1D"            // 存置信道
+#define TXLock      "\x17\x0E"                    // 禁发
+#define ChSave      "\x1A\x1B\x1C\x1D"            // 保存信道
 #define ChDele      "\x1E\x1F\x1C\x1D"            // 删除信道
-#define ChName      "\x7F\x80\x1C\x1D"            // 命名信道
-#define F1Shrt      "\xC5\xD2\xC3\xC4\xA1\xA2"    // 侧键1短按
-#define F1Long      "\xC5\xD2\xC3\xC4\xA1\xA4"    // 侧键1长按
-#define F2Shrt      "\xC5\xD2\xC6\xC4\xA1\xA2"    // 侧键2短按
-#define F2Long      "\xC5\xD2\xC6\xC4\xA1\xA4"    // 侧键2长按
-#define MLong       "\x4D\xA1\xA4"                // M键长按
-#define BatSav      "\x8C\x8D\x0C\x89"            // 省电模式
-#define BatTxt      "\x8D\xBA\x93\x94"            // 电池文本
-#define Mic         "\x8E\x8F\x90\x91\x92"        // 麦克风
-#define ChDisp      "\x1C\x1D\x93\x94\x0C\x89"    // 信道显示模式
-#define POnMsg      "\xC3\xC4\x93\x94"            // 开机显示
-#define BLTime      "\x95\x96\x99\x9A"            // 背光时间
-#define BLMax       "\x97\x98\xBC\xBD"            // 背光最大
-#define Beep        "\xA9\x0B"                    // 响应音
-#define Roger       "\x9B\x9C\x0B"                // 首尾音
+#define ChName      "\x7F\x80\x1C\x1D"            // 信道名称
+#define F1Shrt      "F1\xA1\xA2"                  // F1短按
+#define F1Long      "F1\xA1\xA4"                  // F1长按
+#define F2Shrt      "F2\xA1\xA2"                  // F2短按
+#define F2Long      "F2\xA1\xA4"                  // F2长按
+#define MLong       "M\xA1\xA4"                   // M长按
+#define BatSav      "\x8C\x8D"                    // 省电
+#define BatTxt      "\x8D\xBA\x93\x94"            // 电量显示
+#define Mic         "\x8E\x8F\x90\x91\x92"        // MIC增益
+#define ChDisp      "\x1C\x1D\x93\x94"            // 信道显示
+#define POnMsg      "\xC3\xC4\x93\x94"            // 开机信息
+#define BLTime      "\x95\x96"                    // 背光
+#define BLMax       "\x97\x98\xBC\xBD"            // 亮度
+#define Beep        "\xA9\x0B"                    // 按键音
+#define Roger       "\x9B\x9C\x0B"                // 尾音
 #define STE         "\x9C\x0B\x9D\x1F"            // 尾音消除
-#define RP_STE      "\x9E\x9F\xA0\x9C\x0B\x9D\x1F" // 过中继尾音消除
-#define Call1       "\xA1\xA2\xA3\xA4"            // 按键即呼
-#define VOX         "\xA1\xA2"                    // 声控
-#define SysInf      "\xB5\xB6\xA7\xB8"            // 系统信息
-#define BatVol      "\x8D\xBA\xBC\xBD"            // 电池大小
-#define SetPwr      "\xC7\xD0\xC9\xD0"            // 设置功率
-#define SetCtr      "\xC7\xD0\xB9\x10"            // 设置中心
-#define SetInv      "\xC7\xD0\xB1\xB8"            // 设置反转
-#define SetLck      "\xC7\xD0\xD9\x0E\x0F"        // 设置锁定
-#define SetMet      "\xC7\xD0\xBD\xBD"            // 设置测量
-#define SetGui      "\xC7\xD0\x93\x94"            // 设置GUI
-#define SetNFM      "\xC7\xD0\xC7\xB9"            // 设置NFM
-#define SetVol      "\xC7\xD0\xA9\x0B"            // 设置音量
-#define SetKey      "\xC7\xD0\xA1\xA2"            // 设置按键
-#define SetNWR      "\xC7\xD0\x8E\x8F"            // 设置窄带
-#define FLock       "\x03\xB7\xB8\xB9"            // 频段解锁
+#define RP_STE      "\x9E\x9F\xA0\x9C\x0B\x9D\x1F" // 中继尾音消除
+#define Call1       "\xA1\xA2"                    // 快捷
+#define VOX         "VOX"                        // VOX
+#define SysInf      "\xB5\xB6\xA7\xB8"            // 信息
+#define BatVol      "\xB7\xC1"                    // 功率
+#define SetPwr      "\xBE\x07"                    // 对比度
+#define SetCtr      "\xC7\xD0\xB1\xB8"            // 反色
+#define SetInv      "\xC7\xD0\xD9\x0E\x0F"        // 锁定模式
+#define SetLck      "\xC7\xD0\xBD\xBD"            // S表模式
+#define SetMet      "\xC7\xD0\x93\x94"            // Gui模式
+#define SetGui      "\xC7\xD0\xC7\xB9"            // 窄带
+#define SetNFM      "\x03\xB7\xB8\xB9"            // 频率锁
+#define SetVol      "\x8D\xBA\xBA\xBB\x18"        // 电池校准
+#define SetKey      "\x8D\xBA\xC8\xC9"            // 电池容量
+#define SetNWR      "\xBE\x07\x88\xAC"            // 重置
+#define FLock       "\x03\xB7\xB8\xB9"            // 频率锁
 #define BatCal      "\x8D\xBA\xBA\xBB\x18"        // 电池校准
-#define BatTyp      "\x8D\xBA\xC8\xC9"            // 电池类型
-#define Reset       "\xBE\x07\x88\xAC"            // 参数复位
+#define BatTyp      "\x8D\xBA\xC8\xC9"            // 电池容量
+#define Reset       "\xBE\x07\x88\xAC"            // 重置
 
-// Sub-menu items
-#define WIDE        "\x8E\x8F"                    // 宽带
-#define NARROW      "\x91\x92"                    // 窄带
-#define OFF         "\xC1\xC2"                    // 关闭
-#define ON          "\xC3\xC4"                    // 开启
-#define NA          "\xB1\xB2\xD1"                // 不本（不适用）
+// Sub-menu items - keep in English
+#define WIDE        "WIDE"
+#define NARROW      "NARROW"
+#define OFF         "OFF"
+#define ON          "ON"
+#define NA          "N/A"
 
 #else
 // English mode - keep original English text
--- ui/menu.c
+++ ui/menu.c
@@ -40,119 +40,58 @@
 
 const t_menu_item MenuList[] =
 {
-//   text,          menu ID
     {SQL,          MENU_SQL           },
     {DWMode,       MENU_TDR           },
     {STEP,         MENU_STEP          },
-    {Power,        MENU_TXP           }, // was "TXP"
-    {RxDCS,        MENU_R_DCS         }, // was "R_DCS"
-    {RxCTCS,       MENU_R_CTCS        }, // was "R_CTCS"
-    {TxDCS,        MENU_T_DCS         }, // was "T_DCS"
-    {TxCTCS,       MENU_T_CTCS        }, // was "T_CTCS"
-    {TxDir,        MENU_SFT_D         }, // was "SFT_D"
-    {TxOffs,       MENU_OFFSET        }, // was "OFFSET"
+    {Power,        MENU_TXP           },
+    {RxDCS,        MENU_R_DCS         },
+    {RxCTCS,       MENU_R_CTCS        },
+    {TxDCS,        MENU_T_DCS         },
+    {TxCTCS,       MENU_T_CTCS        },
+    {TxDir,        MENU_SFT_D         },
+    {TxOffs,       MENU_OFFSET        },
     {W_N,          MENU_W_N           },
 #ifdef ENABLE_FEAT_F4HWN
-    {Scramb,       MENU_SCR           }, // was "SCR"
+    {Scramb,       MENU_SCR           },
 #endif
-    {BzLock,       MENU_BCL           }, // was "BCL"
-//    {"Compnd",      MENU_COMPAND       },
-    {Mode,         MENU_AM            }, // was "AM"
+    {BzLock,       MENU_BCL           },
+    {Mode,         MENU_AM            },
 #ifdef ENABLE_FEAT_F4HWN
     {TXLock,       MENU_TX_LOCK       },
 #endif
-//    {"ScAdd1",      MENU_S_ADD1        },
-//    {"ScAdd2",      MENU_S_ADD2        },
-//    {"ScAdd3",      MENU_S_ADD3        },
-    {ChSave,       MENU_MEM_CH        }, // was "MEM-CH"
-    {ChDele,       MENU_DEL_CH        }, // was "DEL-CH"
+    {ChSave,       MENU_MEM_CH        },
+    {ChDele,       MENU_DEL_CH        },
     {ChName,       MENU_MEM_NAME      },
-
-//    {"SList",       MENU_S_LIST        },
-//    {"SList1",      MENU_SLIST1        },
-//    {"SList2",      MENU_SLIST2        },
-//    {"SList3",      MENU_SLIST3        },
-//    {"ScnRev",      MENU_SC_REV        },
-#ifndef ENABLE_FEAT_F4HWN
-    #ifdef ENABLE_NOAA
-        {"NOAA-S",      MENU_NOAA_S    },
-    #endif
-#endif
     {F1Shrt,       MENU_F1SHRT        },
     {F1Long,       MENU_F1LONG        },
     {F2Shrt,       MENU_F2SHRT        },
     {F2Long,       MENU_F2LONG        },
     {MLong,        MENU_MLONG         },
-
-//    {"KeyLck",      MENU_AUTOLK        }, // was "AUTOLk"
-//    {"TxTOut",      MENU_TOT           }, // was "TOT"
-    {BatSav,       MENU_SAVE          }, // was "SAVE"
+    {BatSav,       MENU_SAVE          },
     {BatTxt,       MENU_BAT_TXT       },
     {Mic,          MENU_MIC           },
-
-//    {"MicBar",      MENU_MIC_BAR       }, //去掉了音量条显示
-
-    {ChDisp,       MENU_MDF           }, // was "MDF"
+    {ChDisp,       MENU_MDF           },
     {POnMsg,       MENU_PONMSG        },
-    {BLTime,       MENU_ABR           }, // was "ABR"
-//    {"BLMin",       MENU_ABR_MIN       },
+    {BLTime,       MENU_ABR           },
     {BLMax,        MENU_ABR_MAX       },
-//    {"BLTxRx",      MENU_ABR_ON_TX_RX  },
     {Beep,         MENU_BEEP          },
-//#ifdef ENABLE_VOICE
-//    {"Voice",       MENU_VOICE         },
-//#endif
     {Roger,        MENU_ROGER         },
     {STE,          MENU_STE           },
     {RP_STE,       MENU_RP_STE        },
     {Call1,        MENU_1_CALL        },
-//#ifdef ENABLE_ALARM
-//    {"AlarmT",      MENU_AL_MOD        },
-//#endif
-//#ifdef ENABLE_DTMF_CALLING
-//    {"ANI ID",      MENU_ANI_ID        },
-//#endif
-//    {"UPCode",      MENU_UPCODE        },
-//    {"DWCode",      MENU_DWCODE        },
-//    {"PTT ID",      MENU_PTT_ID        },
-//    {"D ST",        MENU_D_ST          },
-//#ifdef ENABLE_DTMF_CALLING
-//    {"D Resp",      MENU_D_RSP         },
-//    {"D Hold",      MENU_D_HOLD        },
-//#endif
-//    {"D Prel",      MENU_D_PRE         },
-//#ifdef ENABLE_DTMF_CALLING
-//    {"D Decd",      MENU_D_DCD         },
-//    {"D List",      MENU_D_LIST        },
-//#endif
-//    {"D Live",      MENU_D_LIVE_DEC    }, // live DTMF decoder
-#ifndef ENABLE_FEAT_F4HWN
-    #ifdef ENABLE_AM_FIX
-        {"AM Fix",      MENU_AM_FIX        },
-    #endif
-#endif
-    {"VOX",         MENU_VOX           },
+    {"VOX",        MENU_VOX           },
 #ifdef ENABLE_FEAT_F4HWN
-    {SysInf,       MENU_VOL           }, // was "VOL"
+    {SysInf,       MENU_VOL           },
 #else
-    {BatVol,       MENU_VOL           }, // was "VOL"
+    {BatVol,       MENU_VOL           },
 #endif
-
 #ifdef ENABLE_FEAT_F4HWN
     {SetPwr,       MENU_SET_PWR       },
-    //{"SetPTT",      MENU_SET_PTT       },
-    //{"SetTOT",      MENU_SET_TOT       },
-    //{"SetEOT",      MENU_SET_EOT       },
     {SetCtr,       MENU_SET_CTR       },
     {SetInv,       MENU_SET_INV       },
     {SetLck,       MENU_SET_LCK       },
     {SetMet,       MENU_SET_MET       },
     {SetGui,       MENU_SET_GUI       },
-//    {"SetTmr",      MENU_SET_TMR       },
-//#ifdef ENABLE_FEAT_F4HWN_SLEEP
-//    {"SetOff",       MENU_SET_OFF      },
-//#endif
-
 #ifdef ENABLE_FEAT_F4HWN_NARROWER
     {SetNFM,       MENU_SET_NFM       },
 #endif
@@ -163,29 +102,14 @@ const t_menu_item MenuList[] =
     {SetKey,       MENU_SET_KEY       },
 #endif
 #ifdef ENABLE_NOAA
-    {SetNWR,       MENU_NOAA_S    },
+    {SetNWR,       MENU_NOAA_S        },
 #endif
 #endif
-    // hidden menu items from here on
-    // enabled if pressing both the PTT and upper side button at power-on
     {FLock,        MENU_F_LOCK        },
-//#ifndef ENABLE_FEAT_F4HWN
-//    {"Tx 200",      MENU_200TX         }, // was "200TX"
-//    {"Tx 350",      MENU_350TX         }, // was "350TX"
-//    {"Tx 500",      MENU_500TX         }, // was "500TX"
-//#endif
-//    {"350 En",      MENU_350EN         }, // was "350EN"
-//#ifndef ENABLE_FEAT_F4HWN
-//    {"ScraEn",      MENU_SCREN         }, // was "SCREN"
-//#endif
-//#ifdef ENABLE_F_CAL_MENU
-//    {"FrCali",      MENU_F_CALI        }, // reference xtal calibration
-//#endif
-    {BatCal,       MENU_BATCAL        }, // battery voltage calibration
-    {BatTyp,       MENU_BATTYP        }, // battery type 1600/2200mAh
-    {Reset,        MENU_RESET         }, // might be better to move this to the hidden menu items ?
-
-    {"",                              0xff               }  // end of list - DO NOT delete or move this this
+    {BatCal,       MENU_BATCAL        },
+    {BatTyp,       MENU_BATTYP        },
+    {Reset,        MENU_RESET         },
+    {"",           0xff               }
 };
 
 const uint8_t FIRST_HIDDEN_MENU_ITEM = MENU_F_LOCK;
@@ -209,19 +133,6 @@ const char gSubMenu_SFT_D[][4] =
     "-"
 };
 
-#ifdef ENABLE_CHINESE
-const char gSubMenu_W_N[][5] =
-{
-    WIDE,
-    NARROW
-};
-
-const char gSubMenu_OFF_ON[][5] =
-{
-    OFF,
-    ON
-};
-#else
 const char gSubMenu_W_N[][7] =
 {
     "WIDE",
@@ -233,7 +144,6 @@ const char gSubMenu_OFF_ON[][4] =
     "OFF",
     "ON"
 };
-#endif
 
 const char gSubMenu_NA[4] =
 {
@@ -1364,3 +1274,5 @@ void UI_DisplayMenu(void)
 
     ST7565_BlitFullScreen();
 }
+
+
